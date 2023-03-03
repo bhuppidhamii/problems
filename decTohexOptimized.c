440d47c5855431c561420e8d14f57c934bfc2971
bhupendradhami <dhamibhupendra3@gmail.com>
2023-03-03 20:39:50 +0530

    optimized

---
+++ decTohexOptimized.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    long decimalnum, quotient, remainder;
+    int i, j = 0;
+    char hexadecimalnum[100];
+
+    printf("Enter decimal number: ");
+    scanf("%ld", &decimalnum);
+
+    quotient = decimalnum;
+
+    while (quotient != 0)
+    {
+        remainder = quotient % 16;
+        if (remainder < 10)
+        {
+            hexadecimalnum[j] = 48 + remainder;
+        }
+        else
+        {
+            hexadecimalnum[j] = 55 + remainder;
+        }
+        j++;
+
+        quotient = quotient / 16;
+    }
+
+    // display integer into character
+    for (i = j-1; i >= 0; i--)
+        printf("%c", hexadecimalnum[i]);
+    return 0;
+}
